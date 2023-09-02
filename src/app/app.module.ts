69224dbcf9ab177a1ad79df3b352352ed93a806d
Shadwa Mohamed <shadelhady@gmail.com>
2023-09-03 01:27:19 +0300

    ngFor added for skills

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -9,6 +9,7 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillsComponent } from './skills/skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { FooterComponent } from './footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -18,13 +19,10 @@ import { FooterComponent } from './footer/footer.component';
     AboutMeComponent,
     SkillsComponent,
     PortfolioComponent,
-    FooterComponent
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    FontAwesomeModule
-  ],
+  imports: [BrowserModule, FontAwesomeModule, NgbModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.css']
+  styleUrls: ['./skills.component.css'],
 })
 export class SkillsComponent {
-
+  desc: string =
+    'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit tempore totam eveniet voluptates ipsa deleniti dolores voluptatibus neque explicabo molestiae atque excepturi doloremque placeat, similique inciduntrerum, qui amet velit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus harum, fuga sapiente aut, exercitationem, perferendis dolor nobis earum dignissimos enim alias libero fugiat similique repellat exsit. Mollitia, earum vel?';
+  focus: Array<string> = [
+    'UI/UX Deign',
+    'Responsive Design',
+    'Web Deign',
+    'Mobile App Deign',
+  ];
+  skills: Array<any> = [
+    { topic: 'HTML', percent: 97 },
+    { topic: 'CSS', percent: 98 },
+    { topic: 'JavaScript', percent: 80 },
+    { topic: 'React', percent: 20 },
+    { topic: 'Photoshop', percent: 100 },
+    { topic: 'Adobe XD', percent: 60 },
+    { topic: 'Node js', percent: 70 },
+    { topic: 'WordPress', percent: 20 },
+  ];
 }
